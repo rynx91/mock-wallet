68bb0aad5b9c6663521ac4fe9fa08657e75b28a8
rynx91 <joyce_xiaowei@hotmail.com>
2025-06-14 16:51:19 +0800

    add provider

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,7 +1,11 @@
+import { store } from '@/store';
 import { Slot } from 'expo-router';
+import { Provider } from 'react-redux';
 
 export default function RootLayout() {
   return (
-    <Slot />
+    <Provider store={store}>
+      <Slot />
+    </Provider>
   );
 }
